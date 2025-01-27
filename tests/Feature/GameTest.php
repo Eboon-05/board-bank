4c4c18295a563d0947eea99d19cd3a15dcbeb949
Eban Sayago <eban.sayago@protonmail.com>
2025-01-27 04:18:08 +0000

    Split tests code

---
+++ tests/Feature/GameTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\User;
+
+class GameTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_create_game(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->post(route('games.store'), [
+                'code' => '123456',
+                'initial_balance' => 1000
+            ])
+            ->assertFound();
+
+        $this->assertDatabaseHas('games', [
+            'code' => '123456',
+            'initial_balance' => 1000
+        ]);
+
+        $this->assertDatabaseHas('players', [
+            'user_id' => $user->id,
+            'balance' => 1000
+        ]);
+    }
+
+    public function test_user_can_join_game(): void
+    {
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $this->actingAs($user)
+            ->post(route('games.store'), [
+                'code' => '123456',
+                'initial_balance' => 1000
+            ])
+            ->assertFound();
+
+        $this->actingAs($user2)
+            ->post(route('games.join'), [
+                'code' => '123456'
+            ])
+            ->assertFound();
+
+        $this->assertDatabaseHas('players', [
+            'user_id' => $user->id,
+            'balance' => 1000
+        ]);
+
+        $this->assertDatabaseHas('players', [
+            'user_id' => $user2->id,
+            'balance' => 1000
+        ]);
+    }
+}
